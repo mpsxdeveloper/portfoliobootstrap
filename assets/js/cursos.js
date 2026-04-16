6013598875241cc5ac590796557051ad1d4e6215
mpsxdeveloper <98715891+mpsxdeveloper@users.noreply.github.com>
2026-04-16 10:49:13 -0400

    Create cursos.js

---
+++ assets/js/cursos.js
@@ -0,0 +1,51 @@
+var c = [
+    '<div class="alert alert-primary p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold"><i class="fab fa-laravel"></i> Laravel</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>',
+    '<div class="alert alert-success p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold"><i class="fa-brands fa-php"></i> Trilha Linguagem PHP</h5>'+
+    '<p class="mb-0"><span>Conteúdo (Descrição) </span><span class="float-end">00/00/0000 - 0 horas</span></p>'+
+    '<p class="mb-0"><span>Conteúdo (Descrição) </span><span class="float-end">00/00/0000 - 0 horas</span></p>'+
+    '<p class="mb-0"><span>Conteúdo (Descrição) </span><span class="float-end">00/00/0000 - 0 horas</span></p>'+
+    '<p class="mb-0"><span>Conteúdo (Descrição) </span><span class="float-end">00/00/0000 - 0 horas</span></p>'+    
+    '<hr class="mb-0" />'+
+    '<p class="mb-0 text-end"><span>Nome da instituição</span></p>'+
+    '</div>',    
+    '<div class="alert alert-warning p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold">'+
+    '<i class="fa-solid fa-terminal"></i> Algoritmos e Programação'+
+    '</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>',
+    '<div class="alert alert-danger p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold">'+
+    '<i class="fa-solid fa-database"></i> Banco de dados MySQL'+
+    '</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>',
+    '<div class="alert alert-info p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold">'+
+    '<i class="fa-brands fa-bootstrap"></i></i> Bootstrap'+
+    '</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>',
+    '<div class="alert alert-secondary p-2" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold">'+
+    '<i class="fab fa-js-square"></i> JavaScript & JQuery'+
+    '</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>',
+    '<div class="alert alert-light p-2 border border-dark" role="alert">'+
+    '<h5 class="alert-heading text-center fw-bold">'+
+    '<i class="fab fa-css3-alt"></i> HTML & CSS'+
+    '</h5>'+
+    '<p class="mb-0"><span>Nome da instituição</span><span class="float-end">Carga horária: 00 horas</span></p>'+
+    '<p class="mb-0 text-end">Data: 00/0000</p>'+
+    '</div>'
+];
